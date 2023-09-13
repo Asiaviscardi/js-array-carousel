b33bba99bda43fa22dc4ee5d930508727cdfa316
Asiaviscardi <asia.viscardii@gmail.com>
2023-09-13 16:58:49 +0200

    modifiche

---
+++ script.js
@@ -0,0 +1,6 @@
+const items = document.querySelector('items');
+const buttonprecedente = document.querySelector('sopra');
+const buttonsuccessivo = document.querySelector('sotto');
+const img = [
+    'img/01.webp'
+]
